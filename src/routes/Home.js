1edc9c6c6f6d592ba021f50d82b6612e6856081b
Myeong-Seob <69719857+Myeong-Seob@users.noreply.github.com>
2020-09-28 16:48:07 +0900

    GET MOVIE Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,75 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import Movie from "../components/Movie";
+import styled from "styled-components";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  query {
+    movies(limit: 40, rating: 8.8) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+// function ExchangeRates() {
+//   const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error :(</p>;
+
+//   return data.rates.map(({ currency, rate }) => (
+//     <div key={currency}>
+//       <p>
+//         {currency}: {rate}
+//       </p>
+//     </div>
+//   ));
+// }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I Love GraphQL</Subtitle>
+      </Header>
+      {loading ? (
+        <Loading>Loading...</Loading>
+      ) : (
+        data.movies.map((movie) => <Movie key={movie.id} id={movie.id} />)
+      )}
+    </Container>
+  );
+};
